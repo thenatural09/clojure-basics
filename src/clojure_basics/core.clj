0ab16974c78f079fff1d36a07e31bdbc1ed4bad8
Troy <tward4@tulane.edu>
2016-10-31 12:25:30 -0400

    Filter people.csv

--- src/clojure_basics/core.clj
+++ src/clojure_basics/core.clj
@@ -1,6 +1,27 @@
 (ns clojure-basics.core
+  (:require [clojure.string :as str])
   (:gen-class))
 
+(def file-name "people.csv")
+
+(count file-name)
+
 (defn -main [& args]
-  (println "Hello, world!"))
+  (let [people (str/split-lines (slurp file-name))
+        people (map (fn [line]
+                      (str/split line #","))
+                 people)
+        header (first people)
+        people (rest people)
+        people (map (fn [line]
+                      (zipmap header line))
+                 people)
+        people (filter (fn [line]
+                         (= (get line "country")
+                            "Brazil"))
+                 people)
+        people (pr-str people)]
+    (spit "people.edn" people)))
+   
+   
 
